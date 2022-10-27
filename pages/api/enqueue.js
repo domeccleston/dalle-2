f679d79222dcc63325eab1a03a6dd8089342e01e
Dom Eccleston <domeccleston@gmail.com>
2022-10-27 10:53:31 +0100

    update url again and log

--- pages/api/enqueue.js
+++ pages/api/enqueue.js
@@ -4,19 +4,17 @@ const QSTASH_URL = `https://qstash.upstash.io/v1/publish/`;
 
 export default async function handler(req, res) {
   try {
-    const qstashRequest = await fetch(
-      `${process.env.QSTASH_URL + MY_SLOW_API}`,
-      {
-        headers: {
-          "content-type": "application/json",
-          Authorization: `Bearer ${process.env.QSTASH_TOKEN}`,
-        },
-        method: "POST",
-      }
-    );
+    const qstashRequest = await fetch(QSTASH_URL + MY_SLOW_API, {
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${process.env.QSTASH_TOKEN}`,
+      },
+      method: "POST",
+    });
     const { messageId } = await qstashRequest.json();
     res.status(202).json({ message: `Enqueued new task with ID ${messageId}` });
   } catch (error) {
-    res.status(500).json({ error });
+    console.log(error);
+    res.status(500).json(error);
   }
 }
