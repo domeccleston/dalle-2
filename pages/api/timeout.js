12bda1a1f283e0dc0446374820b2e72536d564a8
Dom Eccleston <domeccleston@gmail.com>
2022-10-27 10:46:57 +0100

    update timeout limits

--- pages/api/timeout.js
+++ pages/api/timeout.js
@@ -1,4 +1,4 @@
 export default async function handler(req, res) {
-  await new Promise((resolve) => setTimeout(resolve, 6000));
-  res.status(200).json({ message: 'responding after 6 seconds' });
+  await new Promise((resolve) => setTimeout(resolve, 11000));
+  res.status(200).json({ message: 'responding after 11 seconds' });
 }
