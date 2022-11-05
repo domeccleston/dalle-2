61ac99b544ae08396b595d402d729d39bb318432
Dom Eccleston <domeccleston@gmail.com>
2022-11-05 14:32:25 +0000

    add redis

--- pages/api/callback.js
+++ pages/api/callback.js
@@ -1,6 +1,15 @@
+import { Redis } from "@upstash/redis";
+
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: proces.env.UPSTASH_REDIS_REST_TOKEN,
+});
+
 export default async function handler(req, res) {
   const { body } = req;
   const decoded = atob(body.body);
   console.log(decoded);
+  console.log(body);
+  console.log(redis);
   return res.status(200).send(decoded);
 }
