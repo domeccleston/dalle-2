649b56a80d46c38d0f0ff29425d13bcaf370c833
Dom Eccleston <domeccleston@gmail.com>
2022-11-05 13:11:40 +0000

    add dalle api

---
+++ pages/api/image.js
@@ -0,0 +1,28 @@
+const QSTASH = `https://qstash.upstash.io/v1/publish/`;
+const DALL_E = "https://api.openai.com/v1/images/generations";
+
+export default async function handler(req, res) {
+  const { prompt } = req.query;
+  try {
+    const response = await fetch("QSTASH + DALL_E", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.QSTASH_TOKEN}`,
+        "upstash-forward-Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
+        "Content-Type": "application/json",
+        "Upstash-Callback": `https://${process.env.VERCEL_URL}/api/callback`,
+      },
+      body: JSON.stringify({
+        prompt,
+        n: 1,
+        size: "1024x1024",
+      }),
+    });
+    const json = await response.json();
+    return res.status(202).json(`Enqueued new task with ID ${messageId}`);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: error.message, type: "Internal server error" });
+  }
+}
