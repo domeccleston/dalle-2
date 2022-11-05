b29db63bd3688c498b61dbf3dfe6b4d07110e8fb
Dom Eccleston <domeccleston@gmail.com>
2022-11-05 22:02:07 +0000

    styling fixes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Image from "next/image";
+import cn from "classnames";
 
 import { useInterval } from "../hooks/use-interval";
 
@@ -8,6 +9,7 @@ export default function Home() {
   const [loading, setLoading] = useState(false);
   const [messageId, setMessageId] = useState("");
   const [image, setImage] = useState(null);
+  const [isImageLoading, setIsImageLoading] = useState(false);
 
   useInterval(
     async () => {
@@ -16,6 +18,7 @@ export default function Home() {
       console.log(json);
       if (res.status === 200) {
         setLoading(false);
+        setIsImageLoading(true);
         setImage(json.data[0].url);
       }
     },
@@ -24,33 +27,33 @@ export default function Home() {
 
   async function submitForm(e) {
     e.preventDefault();
+    setLoading(true);
     const response = await fetch(`/api/image?prompt=${prompt}`);
     const json = await response.json();
     setMessageId(json.id);
     console.log(messageId);
-    setLoading(true);
   }
 
   return (
-    <div className="antialiased mx-auto py-20 h-screen bg-gray-100">
+    <div className="antialiased mx-auto px-4 py-20 h-screen bg-gray-100">
       <div className="flex flex-col items-center justify-center">
-        <h1 className="text-5xl tracking-tighter pb-10 font-bold text-gray-900">
+        <h1 className="text-5xl tracking-tighter pb-10 font-bold text-gray-800">
           Dall-E 2 image generator
         </h1>
-        <form className="flex" onSubmit={submitForm}>
+        <form className="flex w-full sm:w-auto flex-col sm:flex-row" onSubmit={submitForm}>
           <input
-            className="shadow-sm rounded-sm px-3 py-2 min-w-[600px]"
+            className="shadow-sm text-gray-700 rounded-sm px-3 py-2 mb-4 sm:mb-0 sm:min-w-[600px]"
             type="text"
             placeholder="Prompt for DALL-E"
             onChange={(e) => setPrompt(e.target.value)}
           />
           <button
-            className="shadow-sm inline-flex justify-center font-medium items-center px-4 bg-green-600 text-gray-100 py-2 ml-2 rounded-md hover:bg-green-500"
+            className="shadow-sm sm:w-[90px] py-2 inline-flex justify-center font-medium items-center px-4 bg-green-600 text-gray-100 sm:ml-2 rounded-md hover:bg-green-700"
             type="Submit"
           >
             {loading && (
               <svg
-                className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                className="animate-spin h-5 w-5 text-white"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
@@ -70,16 +73,23 @@ export default function Home() {
                 ></path>
               </svg>
             )}
-            Submit
+            {!loading ? 'Submit' : ''}
           </button>
         </form>
         <div className="flex items-center justify-center">
           {image && (
-            <img
-              className="mt-10 rounded-md shadow-md"
+            <Image
+              alt={`Dall-E representation of: ${prompt}`}
+              className={cn(
+                "duration-700 ease-in-out mt-10 rounded-md shadow-md",
+                isImageLoading
+                  ? "grayscale blur-2xl"
+                  : "grayscale-0 blur-0"
+              )}
               src={image}
               width="400"
               height="400"
+              onLoadingComplete={() => setIsImageLoading(false)}
             />
           )}
         </div>
