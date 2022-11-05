723867979ebb40f20dc7330e04189e057529d47e
Dom Eccleston <domeccleston@gmail.com>
2022-11-05 16:37:20 +0000

    remove unsued

--- pages/api/test.js
+++
@@ -1,4 +0,0 @@
-export default async function handler(req, res) {
-  console.log(process.env.VERCEL_URL);
-  res.status(200).send("test");
-}
--- pages/api/timeout.js
+++
@@ -1,4 +0,0 @@
-export default async function handler(req, res) {
-  await new Promise((resolve) => setTimeout(resolve, 11000));
-  res.status(200).json({ message: 'responding after 11 seconds' });
-}
