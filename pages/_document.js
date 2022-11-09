9f6dc949b22f196d435f787ba36fbfd4eb084fca
Dom Eccleston <domeccleston@gmail.com>
2022-11-09 19:16:44 +0000

    lighthouse

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,11 @@ export default function Document() {
       <Head />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet"></link>
+      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet"></link>
+      <title>Dall-E 2 AI Image Generator</title>
+      <meta name="description" content="Generate text-to-image art via OpenAI's Dall E 2 model." />
+      <meta name="keywords" content="AI, text2image, dalle, dall-e, openai" />
+      <meta name="author" content="John Doe" />
       <body>
         <Main />
         <NextScript />
