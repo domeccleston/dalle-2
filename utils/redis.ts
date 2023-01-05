9caa836a826c1b0a5ea286b93dfbe6c157c91976
Dom <domeccleston@gmail.com>
2023-01-05 21:08:58 +0000

    update redis

--- utils/redis.ts
+++ utils/redis.ts
@@ -1,8 +1,5 @@
 import { Redis } from "@upstash/redis";
 
-const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
-});
+const redis = Redis.fromEnv();
 
 export default redis;
