858f87b8cdbc6e81c5f455fc89650367d746fa07
Dom <domeccleston@gmail.com>
2023-01-20 10:51:23 +0000

    update redirect page

--- next.config.js
+++ next.config.js
@@ -2,14 +2,5 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ["oaidalleapiprodscus.blob.core.windows.net"],
-  },
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/disabled",
-        permanent: false,
-      },
-    ];
-  },
+  },  
 };
